132008d1a7ee1adad4f99287e167913c70da97c9
SergioAle210 <ore221122@uvg.edu.gt>
2025-11-08 18:00:59 -0600

    FEAT: Se agregaron más gráficas para la función de realtime

--- sim/urban_evolution/realtime_plot.py
+++ sim/urban_evolution/realtime_plot.py
@@ -1,28 +1,70 @@
 import matplotlib.pyplot as plt
 
+
 # Gráficas simples en vivo desde el DataFrame de resultados en crecimiento
 class LivePlotter:
     def __init__(self):
         plt.ion()
-        self.fig, self.axs = plt.subplots(1, 3, figsize=(12, 3.8))
-        self.axs[0].set_title("Renta promedio")
-        self.axs[1].set_title("Tiempo de viaje promedio")
-        self.axs[2].set_title("Desplazamientos/tick")
-        self.lines = [None, None, None]
+        # Ventana 1
+        self.fig1, self.axs1 = plt.subplots(1, 3, figsize=(13, 3.8))
+        self.axs1[0].set_title("Renta promedio")
+        self.axs1[0].set_xlabel("Paso (mes)")
+        self.axs1[0].set_ylabel("Moneda")
+        self.axs1[1].set_title("Tiempo de viaje promedio")
+        self.axs1[1].set_xlabel("Paso (mes)")
+        self.axs1[1].set_ylabel("Minutos")
+        self.axs1[2].set_title("Desplazamientos por paso")
+        self.axs1[2].set_xlabel("Paso (mes)")
+        self.axs1[2].set_ylabel("Hogares")
+        self.lines1 = [None, None, None]
+        # Ventana 2
+        self.fig2, self.axs2 = plt.subplots(1, 3, figsize=(13, 3.8))
+        self.axs2[0].set_title("% población de bajos ingresos")
+        self.axs2[0].set_xlabel("Paso (mes)")
+        self.axs2[0].set_ylabel("Proporción")
+        self.axs2[1].set_title("Dispersión gentrificación")
+        self.axs2[1].set_xlabel("Paso (mes)")
+        self.axs2[1].set_ylabel("Índice")
+        self.axs2[2].set_title("Tasa de vacancia")
+        self.axs2[2].set_xlabel("Paso (mes)")
+        self.axs2[2].set_ylabel("Proporción")
+        self.lines2 = [None, None, None]
 
     def update(self, df):
         x = df["step"].values
-        series = [
+
+        # Ventana 1
+        series1 = [
             df["avg_rent"].values,
             df["avg_travel"].values,
-            df["displacements"].values
+            df["displacements"].values,
         ]
-        for i, y in enumerate(series):
-            if self.lines[i] is None:
-                self.lines[i], = self.axs[i].plot(x, y)
+        labels1 = ["Renta", "Tiempo viaje", "Desplazamientos"]
+        for i, y in enumerate(series1):
+            if self.lines1[i] is None:
+                (self.lines1[i],) = self.axs1[i].plot(x, y, label=labels1[i])
+                self.axs1[i].legend()
             else:
-                self.lines[i].set_xdata(x)
-                self.lines[i].set_ydata(y)
-                self.axs[i].relim()
-                self.axs[i].autoscale_view()
+                self.lines1[i].set_xdata(x)
+                self.lines1[i].set_ydata(y)
+                self.axs1[i].relim()
+                self.axs1[i].autoscale_view()
+
+        # Ventana 2
+        series2 = [
+            df["share_low_income"].values,
+            df["gentr_dispersion"].values,
+            df["vacancy_rate"].values,
+        ]
+        labels2 = ["% bajos ingresos", "Disp. gentrificación", "Vacancia"]
+        for i, y in enumerate(series2):
+            if self.lines2[i] is None:
+                (self.lines2[i],) = self.axs2[i].plot(x, y, label=labels2[i])
+                self.axs2[i].legend()
+            else:
+                self.lines2[i].set_xdata(x)
+                self.lines2[i].set_ydata(y)
+                self.axs2[i].relim()
+                self.axs2[i].autoscale_view()
+
         plt.pause(0.001)
